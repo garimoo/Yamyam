35721886524ea61a87f0c625d207223c4f10b011
donggeon <dkkim04@naver.com>
2016-11-08 20:13:26 +0900

    Initial commit

---
+++ app/AppConfig.java
@@ -0,0 +1,15 @@
+package dongguk.yamyam.app;
+/**
+ * Created by DK on 2016-10-12.
+ */
+public class AppConfig {
+	public static String ip = "192.163.43.62";
+
+	public static String URL_LOGIN = "http://" + ip + "/yamyam_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://" + ip + "/yamyam_api/register.php";
+
+	// Server data input url
+	public static String URL_SEARCH = "http://" + ip + "/yamyam_api/search.php";
+}
